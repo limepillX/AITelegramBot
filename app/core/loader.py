d5b831b8ea7513082ef8c4cd4e5da8cabca89e33
tamu <justtedyt@gmail.com>
2024-06-07 11:29:48 +0200

    Updated code for aiogram 3.7

--- app/core/loader.py
+++ app/core/loader.py
@@ -1,9 +1,12 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from openai import AsyncOpenAI
 
 from app.core.config import config
 
-bot = Bot(token=config.telegram_bot.token, parse_mode="HTML")
+bot = Bot(token=config.bot_token)
+
+bot = Bot(token=config.telegram_bot.token, default=DefaultBotProperties(parse_mode="HTML"))
 dp = Dispatcher()
 
 openai_client = AsyncOpenAI(api_key=config.openai.api_key)
